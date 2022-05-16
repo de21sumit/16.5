c6b21759437991d10c14bd60581146b631b4cae6
de21sumit <de21sumit@gmail.com>
2022-05-16 19:57:22 +0530

    trial 1 commit

---
+++ NamingConventionClass.java
@@ -0,0 +1,35 @@
+package naming.convention;
+
+public class NamingConventionClass {
+
+	public static void main(String[] args) {
+
+int ab = 67;
+int $aa = 78;
+float _pg = 90.45f;
+float u78_23 = 44.5f;
+
+float void = 44.55f;// invalid
+
+int AB = 89;
+int Ab = 66;
+int aB = 67;
+System.out.println(ab);
+int 12bb = 89; // illegal
+float -ggg = 89;
+
+		
+
+	}
+	
+	public static void sample()
+	{
+		
+	}
+	
+	public static void saMple()
+	{
+		
+	}
+
+}
